d72ee38a726c8236f65f7a25674ad0eb726a3aae
Kamenchukov <77745444+Kamenchukov@users.noreply.github.com>
2022-05-12 15:46:23 +0300

    Update AppDelegate.swift

--- TRacker/AppDelegate.swift
+++ TRacker/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        GMSServices.provideAPIKey("AIzaSyA_EP0iaICfuT1_PSnKiTmpGbr4oipcB4k")
+        GMSServices.provideAPIKey("")
         
         return true
     }
