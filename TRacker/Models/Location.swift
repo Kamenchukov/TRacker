261317a921987143fae206336893af2776bcda78
Constantin <k.kamenchukov@gmail.com>
2022-05-12 15:40:25 +0300

    HW_2_update

--- TRacker/Models/Location.swift
+++ TRacker/Models/Location.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import CoreLocation
+import RealmSwift
+
+class Location: Object {
+    @objc dynamic var latitude = 0.0
+    @objc dynamic var longitude = 0.0
+
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(
+            latitude: latitude,
+            longitude: longitude)
+    }
+    
+    convenience init(latitude: Double, longitude: Double) {
+        self.init()
+        
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
--- TRacker/Realm/RouteStorage.swift
+++ TRacker/Realm/RouteStorage.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import RealmSwift
+
+class RouteStorage {
+    
+    static var shared = RouteStorage()
+    
+    private var realm = try! Realm()
+    
+    private init() {  }
+
+    
+    func saveLastRoute(route: [Location]) {
+        clearLastRoute()
+        try? realm.write {
+            realm.add(route)
+        }
+    }
+
+    func loadLastRoute() -> Results<Location> {
+        return realm.objects(Location.self)
+    }
+
+    func clearLastRoute() {
+        let result = realm.objects(Location.self)
+        try? realm.write {
+            realm.delete(result)
+        }
+    }
+}
